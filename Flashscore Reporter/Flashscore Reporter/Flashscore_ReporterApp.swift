97f35d20328e77118430ed4c57a96afc23914963
Antoni Gebauer <antoni.gebauer@gmail.com>
2024-10-20 18:57:30 +0200

    main - initial commit

---
+++ Flashscore Reporter/Flashscore Reporter/Flashscore_ReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashscore_ReporterApp.swift
+//  Flashscore Reporter
+//
+//  Created by Antoni Gebauer on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flashscore_ReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
