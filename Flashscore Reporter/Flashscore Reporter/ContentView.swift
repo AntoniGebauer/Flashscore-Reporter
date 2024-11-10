cd65b45425a94f56ce246638f9722e2da05e632c
Antoni Gebauer <antoni.gebauer@gmail.com>
2024-11-10 09:21:44 +0100

    main - added login view

--- Flashscore Reporter/Flashscore Reporter/ContentView.swift
+++ Flashscore Reporter/Flashscore Reporter/ContentView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Text("Logowanie")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 30)
+                
+                TextField("Nazwa Użytkownika", text: $username)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                if !username.isEmpty {
+                    SecureField("Hasło", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                }
+               
+                
+                Button(action: {
+                    // Add your login logic here
+                }) {
+                    Text("Zaloguj")
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 50)
+                        .cornerRadius(10)
+                }
+                .tint(.black.opacity(0.6))
+                .buttonStyle(.borderedProminent)
+                .disabled(username.isEmpty || password.isEmpty)
+                .padding(.top)
+            }
+            .animation(.easeInOut(duration: 0.4), value: username)
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ Flashscore Reporter/Flashscore Reporter/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Flashscore Reporter
+//
+//  Created by Antoni Gebauer on 10/11/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
